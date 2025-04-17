baaf1381c4421688963fff0b97f3505cf2f9ffbf
La Jota <raphalopes26800@gmail.com>
2025-04-17 16:33:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php include '../includes/header.php'; ?>
+<?php include 'includes/header.php'; ?>
 
 <main>
     <h1>Corps de la page</h1>
-    <!-- Contenu principal de la page -->
 </main>
 
-<?php include '../includes/footer.php'; ?>
+<?php include 'includes/footer.php'; ?>
